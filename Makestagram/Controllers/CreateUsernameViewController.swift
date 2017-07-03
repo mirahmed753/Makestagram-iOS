9156fe9f82aa35cbc18bcbd2fe202e074088db81
Mir Ahmed <mirahmed753@gmail.com>
2017-07-03 14:08:23 -0400

    Updated Login and create Username screens

---
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Mir Ahmed on 7/3/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class CreateUsernameViewController: UIViewController {
+    // Properties
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    // VC Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nextButton.layer.cornerRadius = 6
+    }
+    
+    // Actions
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        guard let firUser = Auth.auth().currentUser,
+            let username = usernameTextField.text,
+            !username.isEmpty else { return }
+        
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else {
+                // handle error
+                return
+            }
+            
+            User.setCurrent(user)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            if let initialViewController = storyboard.instantiateInitialViewController() {
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+        
+//        UserService.create(firUser, username: username) { (user) in
+//            guard let _ = user else {
+//                return
+//            }
+//            
+//            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+//            
+//            if let initialViewController = storyboard.instantiateInitialViewController() {
+//                self.view.window?.rootViewController = initialViewController
+//                self.view.window?.makeKeyAndVisible()
+//            }
+//        }
+    }
+}
---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,94 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Mir Ahmed on 6/30/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+// typealias aliasName = existingType
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController {
+    
+    // PROPERTIES (VARS)
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // VC LIFECYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // do any additional set up after loading the view
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // dispose of any resources that can be recreated
+    }
+    
+    // ACTIONS
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        
+        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                User.setCurrent(user)
+                
+                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+                if let initialViewController = storyboard.instantiateInitialViewController() {
+                    self.view.window?.rootViewController = initialViewController
+                    self.view.window?.makeKeyAndVisible()
+                }
+            } else {
+                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+            }
+        })
+        
+        
+//        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+//            if let _ = User(snapshot: snapshot) {
+//                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+//                
+//                if let initialViewController = storyboard.instantiateInitialViewController() {
+//                    self.view.window?.rootViewController = initialViewController
+//                    self.view.window?.makeKeyAndVisible()
+//                }
+//            } else {
+//                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+//            }
+//        })
+    }
+}
