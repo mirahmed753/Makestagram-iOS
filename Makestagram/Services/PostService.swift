eee5909d02a384729ef40205ed9ed8e0c570245d
Mir Ahmed <mirahmed753@gmail.com>
2017-07-05 11:38:49 -0400

    Created Service files to assist in uploading pictures to Firebase

---
+++ Makestagram/Services/PostService.swift
@@ -0,0 +1,26 @@
+//
+//  PostService.swift
+//  Makestagram
+//
+//  Created by Mir Ahmed on 7/5/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseStorage
+import FirebaseDatabase
+
+struct PostService {
+    static func create(for image: UIImage) {
+        let imageRef = Storage.storage().reference().child("test_image.jpg")
+        StorageService.uploadImage(image, at: imageRef) { (downloadURL) in
+            guard let downloadURL = downloadURL else {
+                return
+            }
+            
+            let urlString = downloadURL.absoluteString
+            print("image url: \(urlString)")
+        }
+    }
+}
