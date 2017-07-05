55559d5e3e29221b02e51407e415f3e37cc27975
Mir Ahmed <mirahmed753@gmail.com>
2017-07-05 16:30:10 -0400

    Created a PostTableViewCell to display images

---
+++ Makestagram/Views/PostImageCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by Mir Ahmed on 7/5/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    // subviews
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    
+    // cell lifecycle 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
